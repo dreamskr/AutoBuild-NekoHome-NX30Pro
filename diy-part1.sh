c0b7268becb2114ae1dd741895b9923cab608a09
dreamskr <43511466+dreamskr@users.noreply.github.com>
2025-11-11 19:42:38 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# passwall
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+
+# iStore
+echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# add feed
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
